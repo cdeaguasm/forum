5b67c6635895365dab50cbb13c2c17d80cb5548d
Carlos De Aguas <cdeaguasm@gmail.com>
2019-02-21 22:07:38 -0500

    Implementing post service

---
+++ Forum/Forum/Controllers/PostController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data.Interfaces;
+using Data.Models;
+using Forum.Models.Post;
+using Forum.Models.Reply;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        // GET: Post
+        public ActionResult Index(int id)
+        {
+            var post = _postService.GetById(id);
+            var replies = BuildPostReplies(post.Replies);
+
+            var model = new PostIndexViewModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
+                AuthorImageUrl = post.User.ProfileImageUrl,
+                AuthorRating = post.User.Rating,
+                Created = post.Created,
+                PostContent = post.Content,
+                Replies = replies
+            };
+            return View(model);
+        }
+
+        private IEnumerable<PostReplyViewModel> BuildPostReplies(IEnumerable<PostReply> replies)
+        {
+            return replies.Select(r => new PostReplyViewModel
+            {
+                Id = r.Id,
+                AuthorName = r.User.UserName,
+                AuthroId = r.User.Id,
+                AuthorImageUrl = r.User.ProfileImageUrl,
+                AuthorRating = r.User.Rating,
+                Created = r.Created,
+                ReplyContent = r.Content
+            });
+        }
+
+        // GET: Post/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Post/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Post/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Post/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Post/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Forum/Forum/Models/Post/PostIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Forum.Models.Reply;
+using System;
+using System.Collections.Generic;
+
+namespace Forum.Models.Post
+{
+    public class PostIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorImageUrl { get; set; }
+        public int AuthorRating { get; set; }
+        public DateTime Created { get; set; }
+        public string PostContent { get; set; }
+
+        public IEnumerable<PostReplyViewModel> Replies { get; set; }
+    }
+}
