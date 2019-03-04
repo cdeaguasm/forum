0db404e5620cb24b11125c605ad71cb207cedcf4
Carlos De Aguas <cdeaguasm@gmail.com>
2019-03-04 08:54:55 -0500

    Adding styles in forum topic

--- Forum/Forum/Models/Forum/ForumTopicViewModel.cs
+++ Forum/Forum/Models/Forum/ForumTopicViewModel.cs
@@ -7,5 +7,6 @@ namespace Forum.Models.Forum
     {
         public ForumListingViewModel Forum { get; set; }
         public IEnumerable<PostListingViewModel> Posts { get; set; }
+        public string SearchQuery { get; set; }
     }
 }
